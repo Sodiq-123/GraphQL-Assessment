144b561809e01f58eb7c75091688034bde551ede
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2022-02-23 11:10:01 +0100

    modified

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,12 +1,12 @@
-import Emittery from 'emittery'
-import { sendEmail, ISendMail } from '../helpers/sendMail'
+// import Emittery from 'emittery'
+// import { sendEmail, ISendMail } from '../helpers/sendMail'
 
-const emitter = new Emittery<{
-  'send:email': ISendMail
-}>()
+// const emitter = new Emittery<{
+//   'send:email': ISendMail
+// }>()
 
-emitter.on('send:email', ({ body, to }) => {
-  sendEmail(to, body, 'Verify your email')
-})
+// emitter.on('send:email', ({ body, to }) => {
+//   sendEmail(to, body, 'Verify your email')
+// })
 
-export default emitter
+// export default emitter
